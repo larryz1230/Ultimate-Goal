64739e262f137025e94df3bfed82f6928f989af1
GrapeJuiceSoda <kcnanmoto@berkeley.edu>
2020-10-31 17:05:26 -0700

    Sample Trajecotry

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/trajectory/trajectory.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.Auto.trajectory;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+public class trajectory extends LinearOpMode {
+    @Override
+    public void runOpMode(){
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        Trajectory myTrajectory = drive.trajectoryBuilder(new Pose2d())
+                .splineTo(new Vector2d(5, 8), 0)
+                .splineTo(new Vector2d(9, 8), 0)
+                .build();
+
+        waitForStart();
+        if(isStopRequested()) return;
+
+        drive.followTrajectory(myTrajectory);
+    }
+}
