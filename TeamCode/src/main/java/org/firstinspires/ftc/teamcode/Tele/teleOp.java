870639fd209ddfed71aa2130002f27a9ca881e82
GrapeJuiceSoda <kendallcarmel@gmail.com>
2020-11-11 23:30:07 -0800

    Make packages for orginization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/teleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/teleOp.java
@@ -1,33 +1,36 @@
 package org.firstinspires.ftc.teamcode.Tele;
+
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.button.Button;
 import com.arcrobotics.ftclib.command.button.GamepadButton;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.subsystems.*;
-import org.firstinspires.ftc.teamcode.commands.*;
+import org.firstinspires.ftc.teamcode.commands.SetPower;
+import org.firstinspires.ftc.teamcode.subsystems.MotorSubsystem;
 
-@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "Teleop")
+@TeleOp
 public class teleOp extends CommandOpMode {
-    private shooterSubsystem shooter_one;
-    private shooterSubsystem shooter_two;
+    private MotorSubsystem shooter;
+    private MotorSubsystem passthrough;
 
     @Override
     public void initialize() {
-        shooter_one = new shooterSubsystem(hardwareMap, "test_shooter_one");
-        shooter_two = new shooterSubsystem(hardwareMap, "test_shooter_two");
-        GamepadEx shooter_driver = new GamepadEx(gamepad1);
+        shooter = new MotorSubsystem(hardwareMap, "shooter");
+        passthrough = new MotorSubsystem(hardwareMap, "passthrough");
 
-        Button shooterOneStart = (new GamepadButton(shooter_driver, GamepadKeys.Button.Y))
-                .whenPressed(new shooterCommand(shooter_one, 1));
-        Button shooterOneEnd = (new GamepadButton(shooter_driver, GamepadKeys.Button.B))
-                .whenPressed(new shooterCommand(shooter_one, 0));
-        Button shooterTwoStart = (new GamepadButton(shooter_driver, GamepadKeys.Button.Y))
-                .whenPressed(new shooterCommand(shooter_two, 1));
-        Button shooterTwoEnd = (new GamepadButton(shooter_driver, GamepadKeys.Button.Y))
-                .whenPressed(new shooterCommand(shooter_two, 0));
+        GamepadEx driverOp = new GamepadEx(gamepad1);
 
-        register(shooter_one, shooter_two);
+        Button shooterStart = (new GamepadButton(driverOp, GamepadKeys.Button.Y))
+                .whenPressed(new SetPower(shooter, 1));
+        Button shooterEnd = (new GamepadButton(driverOp, GamepadKeys.Button.B))
+                .whenPressed(new SetPower(shooter, 0));
+        Button passthroughStart = (new GamepadButton(driverOp, GamepadKeys.Button.X))
+                .whenPressed(new SetPower(passthrough, 1));
+        Button passthroughEnd = (new GamepadButton(driverOp, GamepadKeys.Button.A))
+                .whenPressed(new SetPower(passthrough, 0));
+
+        register(shooter, passthrough);
     }
 }
