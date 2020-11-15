6f7bb28eec83385811185d7b2f3ae840f2a5db0c
GrapeJuiceSoda <kendallcarmel@gmail.com>
2020-11-15 10:14:42 -0800

    Field Centric Complete

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
@@ -2,40 +2,50 @@ package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.hardware.GyroEx;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 
 public class DriveSubsystem extends SubsystemBase {
     private final MecanumDrive m_drive;
 
     private final MotorEx m_frontLeft, m_frontRight, m_bottomLeft, m_bottomRight;
+    private final GyroEx m_gyro;
 
 
-    public DriveSubsystem(MotorEx leftMotorTop, MotorEx rightMotorTop, MotorEx leftMotorBottom, MotorEx rightMotorBottom) {
+    public DriveSubsystem(MotorEx leftMotorTop, MotorEx rightMotorTop, MotorEx leftMotorBottom, MotorEx rightMotorBottom, GyroEx gyroEx) {
         this.m_frontLeft = leftMotorTop;
         this.m_frontRight = rightMotorTop;
         this.m_bottomLeft = leftMotorBottom;
         this.m_bottomRight = rightMotorBottom;
+        this.m_gyro = gyroEx;
 
-        this.m_drive = new MecanumDrive(this.m_frontLeft,
+        this.m_frontLeft.setRunMode(Motor.RunMode.RawPower);
+        this.m_frontRight.setRunMode(Motor.RunMode.RawPower);
+        this.m_bottomLeft.setRunMode(Motor.RunMode.RawPower);
+        this.m_bottomRight.setRunMode(Motor.RunMode.RawPower);
+
+        this.m_drive = new MecanumDrive(
+                this.m_frontLeft,
                 this.m_frontRight,
                 this.m_bottomLeft,
                 this.m_bottomRight
         );
+        m_drive.setMaxSpeed(1);
     }
 
-    public void drive(double hSpeed, double vSpeed) {
-        double tSpeed = (hSpeed + vSpeed) / 2;
-//        m_drive.driveFieldCentric(
-//                hSpeed,
-//                vSpeed,
-//                tSpeed,
-//                gyroAngle
-//        );
-        m_drive.driveRobotCentric(
+    public void drive(double hSpeed, double vSpeed, double tSpeed, double gyroAngle) {
+        m_drive.driveFieldCentric(
                 hSpeed,
                 vSpeed,
-                tSpeed
+                tSpeed,
+                gyroAngle
         );
+//        m_drive.driveRobotCentric(
+//                hSpeed,
+//                vSpeed,
+//                tSpeed
+//        );
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele/testMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele/testMotor.java
@@ -5,6 +5,8 @@ import com.arcrobotics.ftclib.command.button.Button;
 import com.arcrobotics.ftclib.command.button.GamepadButton;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.commands.SetPower;
@@ -14,22 +16,26 @@ import org.firstinspires.ftc.teamcode.subsystems.MotorSubsystem;
 public class testMotor extends CommandOpMode {
     private MotorSubsystem shooter;
     private MotorSubsystem passthrough;
+    private MotorSubsystem grabber;
+    private MotorSubsystem lift;
 
     @Override
     public void initialize() {
-        shooter = new MotorSubsystem(hardwareMap, "shooter");
-        passthrough = new MotorSubsystem(hardwareMap, "passthrough");
+        shooter = new MotorSubsystem(hardwareMap, new MotorEx(hardwareMap, "frontLeft", Motor.GoBILDA.RPM_435));
+        passthrough = new MotorSubsystem(hardwareMap, new MotorEx(hardwareMap, "frontRight", Motor.GoBILDA.RPM_435));
+        grabber = new MotorSubsystem(hardwareMap, new MotorEx(hardwareMap, "bottomLeft", Motor.GoBILDA.RPM_435));
+        lift = new MotorSubsystem(hardwareMap, new MotorEx(hardwareMap, "bottomRight", Motor.GoBILDA.RPM_435));
 
         GamepadEx driverOp = new GamepadEx(gamepad1);
 
         Button shooterStart = (new GamepadButton(driverOp, GamepadKeys.Button.Y))
-                .whenPressed(new SetPower(shooter, 1));
+                .whenPressed(new SetPower(shooter, -1));
         Button shooterEnd = (new GamepadButton(driverOp, GamepadKeys.Button.B))
-                .whenPressed(new SetPower(shooter, 0));
-        Button passthroughStart = (new GamepadButton(driverOp, GamepadKeys.Button.X))
                 .whenPressed(new SetPower(passthrough, 1));
+        Button passthroughStart = (new GamepadButton(driverOp, GamepadKeys.Button.X))
+                .whenPressed(new SetPower(grabber, -1));
         Button passthroughEnd = (new GamepadButton(driverOp, GamepadKeys.Button.A))
-                .whenPressed(new SetPower(passthrough, 0));
+                .whenPressed(new SetPower(lift, 1));
 
         register(shooter, passthrough);
     }
