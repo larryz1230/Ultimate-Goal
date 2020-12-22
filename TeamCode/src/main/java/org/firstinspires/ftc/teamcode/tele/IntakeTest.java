620f9ddf2b7a3fe7093e809e9465b42006fdce58
Anas Saeed <saeedanas396@gmail.com>
2020-12-21 18:12:25 -0800

    Added Velo PID tuning for dual motor flywheel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele/IntakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele/IntakeTest.java
@@ -5,21 +5,28 @@ import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.commands.SetIntakePower;
+import org.firstinspires.ftc.teamcode.commands.SetShootPower;
 import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.BevelShooterSubsystem;
 
 @TeleOp
 public class IntakeTest extends CommandOpMode {
-    private IntakeSubsystem intake;
-    private SetIntakePower intakeCommand;
+  private IntakeSubsystem intake;
+  private SetIntakePower intakeCommand;
+  private BevelShooterSubsystem shooter;
+  private SetShootPower shootCommand;
 
-    @Override
-    public void initialize() {
-        intake = new IntakeSubsystem( new MotorEx(hardwareMap, "intake"));
+  @Override
+  public void initialize() {
+    intake = new IntakeSubsystem(new MotorEx(hardwareMap, "intake"));
+    shooter = new BevelShooterSubsystem(new MotorEx(hardwareMap, "motor"), new MotorEx(hardwareMap, "inverted"));
 
-        GamepadEx driverOp = new GamepadEx(gamepad1);
-        intakeCommand = new SetIntakePower(intake, driverOp::getLeftY);
+    GamepadEx driverOp = new GamepadEx(gamepad1);
+    intakeCommand = new SetIntakePower(intake, driverOp::getLeftY);
+    shootCommand = new SetShootPower(shooter, driverOp::getRightY);
 
-        schedule(intakeCommand);
-        register(intake);
-    }
+    schedule(intakeCommand, shootCommand);
+    register(intake, shooter);
+  }
 }
+
